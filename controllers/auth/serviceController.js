64097e3ddd7be0d1912bab0e86ded228ba1d3b30
Akash Jogani <akashjogani93@gmail.com>
2026-07-08 13:37:28 +0530

    test cases

--- controllers/auth/serviceController.js
+++ controllers/auth/serviceController.js
@@ -22,7 +22,7 @@ const serviceController = {
 
             const isOtpLogin = !!value.phone_number && !!value.otp;
             if (isOtpLogin) {
-                const userQuery = `SELECT user_id, username, email, phone_number, user_type as Role,password,CONCAT_WS(' ', first_name, middle_name, last_name) AS full_name FROM users WHERE is_deleted = 0 AND phone_number = '${value.phone_number}'`;
+                const userQuery = `SELECT user_id,user_custum_id, username, email, phone_number, user_type as Role,password,CONCAT_WS(' ', first_name, middle_name, last_name) AS full_name FROM users WHERE is_deleted = 0 AND phone_number = '${value.phone_number}'`;
                 const users = await getData(userQuery, next);
                 if (!users || users.length === 0) {
                     return next(CustomErrorHandler.doesNotExist("Mobile number not registered"));
@@ -68,6 +68,7 @@ const serviceController = {
                 SELECT
                     user_id,
                     username,
+                    user_custum_id,
                     email,
                     phone_number,
                     user_type AS Role,
